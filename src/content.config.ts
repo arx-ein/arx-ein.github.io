56108087face2775a647c2a9a8fea75a7bdfb512
arx-ein <arxein7@gmail.com>
2026-03-27 20:49:57 +0900

    Upgrade dependencies (Astro 6)

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import {defineCollection} from "astro:content";
+import {z} from "astro/zod";
+import {glob} from "astro/loaders";
+
+const blogCollection = defineCollection({
+  loader: glob({pattern: "**/*.(md|mdx)", base: "./src/content/blog"}),
+  schema: z.object({
+    create: z.date(),
+    update: z.date().nullable(),
+    title: z.string(),
+    caption: z.string().nullable(),
+    description: z.string(),
+    thumbnail: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
